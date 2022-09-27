eb4dc480c298dd5852523cb189c37a781b110428
maxhess22  email = ma.hessalvarez@duocuc.cl [credential]  helper = cache <max@LAPTOP-6MO6UKJU.localdomain>
2022-09-27 14:07:00 -0300

    cambio a la metodologia bem, cambio de nombres de clases, ajuste en algunas secciones de tamaño de fuente y creacion de footer

---
+++ Assets/js/index.js
@@ -0,0 +1,8 @@
+
+const fecha = new Date();
+
+var dia = fecha.getDate();
+var mes = fecha.getMonth();
+var year = fecha.getFullYear()
+
+console.log(`èl dia de hoy es ${dia} el mes es : ${mes} y el año es ${year}` )
