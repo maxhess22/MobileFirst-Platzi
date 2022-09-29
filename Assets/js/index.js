c3b7de278bd7c95e122d6f9071cebc235ebffe8a
maxhess22  email = ma.hessalvarez@duocuc.cl [credential]  helper = cache <max@LAPTOP-6MO6UKJU.localdomain>
2022-09-29 17:38:23 -0300

    creacion de demo slider seccion 3

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,4 +1,26 @@
 let prom_1 = document.getElementById("card-1");
 let prom_2 = document.getElementById("card-2");
 let prom_3 = document.getElementById("card-3");
+let buttonAfter = document.querySelector("#siguiente");
+let buttonBefore = document.querySelector("#retroceder");
+
+
+
+buttonAfter.addEventListener('click', avanzar);
+buttonBefore.addEventListener('click', retroceder);
+
+
+function avanzar () {
+    prom_1.style.display = 'none';
+    prom_2.style.display = 'none';
+    prom_3.style.display = 'flex';
+    prom_3.style.animationName = 'opacitys';
+}
+function retroceder (){
+    prom_3.style.display = 'none';
+    prom_2.style.display = 'none';
+    prom_1.style.display = 'flex';
+    prom_1.style.animationName = 'opacitys';
+}
+
 
